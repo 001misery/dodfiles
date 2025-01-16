e653e61197b2f3d4994fee1d6d63ed39d7e6bc1f
misery <you@example.com>
2025-01-16 10:56:54 +0000

    added nvim

---
+++ nvim/.config/init.lua
@@ -0,0 +1,7 @@
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.cmd("colorscheme sorbet")
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
